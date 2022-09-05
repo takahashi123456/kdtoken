6fc6e617ecb80c623d7207da93092c31a461183a
Inazawa <takahashinishikigoi@gmail.com>
2022-09-05 11:18:03 +0900

    ヘッダー追加

--- kdtoken/src/rooting/top.js
+++ kdtoken/src/rooting/top.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import Header from '../components/header/header';
 
-export default class Top extends React.Component {
-    render() {
-        return (
+const sections = [
+    { title: '過去の予想', url: '#' },
+    { title: 'モデル別回収率一覧', url: '#' },
+    { title: '今週のレース予想', url: '#' },
+    { title: 'お知らせ', url: '#' },
+    { title: 'お問合せ', url: '#' },
+
+];
+export default function Top() {
+    return (
+        <div>
+            <Header title="競馬AI" sections={sections} />
             <div>
                 <h1>Hello, world!</h1>
             </div>
-        )
-    }
+        </div>
+    )
 }
