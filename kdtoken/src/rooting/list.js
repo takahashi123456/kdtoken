ade3503bda9404756a7c46f839340d914673b234
Inazawa <takahashinishikigoi@gmail.com>
2022-09-05 10:04:15 +0900

    react

---
+++ kdtoken/src/rooting/list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class List extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!(List Page)</h1>
+            </div>
+        )
+    }
+}
---
+++ kdtoken/src/rooting/top.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Top extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+            </div>
+        )
+    }
+}
