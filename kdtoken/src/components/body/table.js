9a848bfe94b4972828fdf7db7ae504ba2e681d94
Inazawa <takahashinishikigoi@gmail.com>
2022-09-07 14:35:41 +0900

    テーブル部分の追加

--- kdtoken/src/components/body/table.js
+++ kdtoken/src/components/body/table.js
@@ -6,30 +6,35 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import Data from '../../prediction.json';
+
+
+const name = Data.horsename;
+const score = Data.Score;
+const favorite = Data.favorite;
+const horsenumber = Data.horsenumber;
+const age = Data.age;
+
 
 function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs, protein };
 }
-
-const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-];
-
+const rows = [];
+for (let i = 0; i < 9; i++) {
+    const a = createData(name[i], score[i], favorite[i], horsenumber[i], age[i]);
+    rows.push(a)
+}
 export default function BasicTable() {
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="right">Calories</TableCell>
-                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell align="right">Score</TableCell>
+                        <TableCell align="right">馬人気</TableCell>
+                        <TableCell align="right">ホースナンバー</TableCell>
+                        <TableCell align="right">うま年齢</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
