0369c84dc7bd5e79fa7f9886366ffa8903a0354f
Inazawa <takahashinishikigoi@gmail.com>
2022-09-12 11:17:48 +0900

    test github.io_17

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactああああああああああああああ
         </a>
 
       </header>
--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ root.render(
   <React.StrictMode>
     <BrowserRouter >
       <Routes >
-        <Route path="/" element={<App />} />
-        <Route path="/top" element={<Top />} />
+        <Route path="/top" element={<App />} />
+        <Route path="/" element={<Top />} />
         <Route path="/list" element={<List />} />
       </Routes>
     </BrowserRouter>
